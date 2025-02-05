b5e394e0eaa27b567a61484a63f6a3309175c30a
Najimuddin Shaikh <sherlockshaikh@gmail.com>
2025-02-05 13:41:00 +0530

    files added

--- day01/emp.js
+++ day01/emp.js
@@ -15,7 +15,7 @@ const employees = [
   activeEmployees=employees.filter((employees)=>(employees.status==="active"))
   sortedSalary=activeEmployees.sort((a,b)=>b.activeEmployees-a.activeEmployees)
   grpByDepartment=Object.groupBy(sortedSalary,({department})=>department)
-  combinedData=
+  combinedData=grpByDepartment.push(...departmentBudgets)
 //   console.log(grpByDepartment)
   console.log(combinedData)
 
