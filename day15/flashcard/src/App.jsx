167f1635e81ba8323a9b5abec505631740f6810c
Najimuddin Shaikh <sherlockshaikh@gmail.com>
2025-02-19 14:58:50 +0530

    files added

---
+++ day15/flashcard/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import FlashCard from './components/FlashCard'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <FlashCard/>
+      </div>
+    </>
+  )
+}
+
+export default App
